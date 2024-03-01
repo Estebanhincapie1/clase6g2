3becea4a1aaa15cf628d0d1184028b0046c3dcaf
Estebanhincapie1 <esteban.hincapie1@udea.edu.co>
2024-03-01 15:56:40 -0500

    cometarios finales

---
+++ archivoPruebasRapidas.py
@@ -0,0 +1,15 @@
+nombre = 'jUAN ESTEBAN hincapie'
+cadena = input('ingrese el nombre:')
+
+print(cadena.isnumeric())
+
+# if cadena in nombre.lower():
+#     print(nombre)
+# elif cadena.lower().split(' ')[1] in nombre.lower() in nombre.lower() or cadena.lower().split(' ')[2] in nombre.lower().split(' ') or cadena.lower().split(' ')[4] in nombre.lower().split(' ') :
+#     print(nombre)
+# else:
+#     print('El nombre ingresado no se encuentra')
+
+
+
+
